56b91762a28a10fb3ecf95d3174087a2ebc96eb9
Waken-dream <94945653+Waken-dream@users.noreply.github.com>
2023-06-13 12:32:07 +0800

    Add files via upload
    Signed-off-by: Waken-dream <94945653+Waken-dream@users.noreply.github.com>

--- helpers.cpp
+++ helpers.cpp
@@ -1,5 +1,6 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
+#include <gmp.h>
 
 using namespace std;
 
@@ -22,9 +23,70 @@ bool is_prime(int n) {
     return true;
 }
 
+std::string convertToBase7(const mpz_t& number) {
+    if (mpz_cmp_si(number, 0) == 0) {
+        return "0";
+    }
+
+    std::string result = "";
+    bool isNegative = false;
+
+    mpz_t temp;
+    mpz_init(temp);
+    mpz_set(temp, number);
+
+    if (mpz_cmp_si(temp, 0) < 0) {
+        isNegative = true;
+        mpz_abs(temp, temp);
+    }
+
+    while (mpz_cmp_si(temp, 0) > 0) {
+        mpz_t remainder;
+        mpz_init(remainder);
+        mpz_mod_ui(remainder, temp, 7);
+        result = mpz_get_str(nullptr, 10, remainder) + result;
+        mpz_fdiv_q_ui(temp, temp, 7);
+        mpz_clear(remainder);
+    }
+
+    if (isNegative) {
+        result = "-" + result;
+    }
+
+    mpz_clear(temp);
+
+    return result;
+    }
+
+std::string convertFromBase7(const std::string& base7Number) {
+    mpz_t number;
+    mpz_init(number);
+    mpz_set_str(number, base7Number.c_str(), 7);
+
+    std::string result = mpz_get_str(nullptr, 10, number);
+
+    mpz_clear(number);
+
+    return result;
+}
+
 namespace py = pybind11;
 
 PYBIND11_MODULE(helpers, m) {
     m.def("extended_gcd", &extended_gcd, "Extended euclid algorithm, output [g,a,b] satisfy ax+by=gcd(a,b)=g");
     m.def("is_prime", &is_prime, "Check if a number is prime.");
+    m.def("convertToBase7", [](const std::string& number) {
+        mpz_t num;
+        mpz_init(num);
+        mpz_set_str(num, number.c_str(), 10);
+
+        std::string result = convertToBase7(num);
+
+        mpz_clear(num);
+
+        return result;
+    });
+    m.def("convertFromBase7", [](const std::string& base7Number) {
+        return convertFromBase7(base7Number);
+    });
 }
